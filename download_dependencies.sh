02717f8a5e472ce01647d60f3f4a293596e56b50
theantisocialextrovert <imrohan0124@gmail.com>
2020-07-11 03:48:48 -0400

    added a script for downloading all the dependencies with few changes

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+printf "[-] instaling hostapd. . .\n"
 apt-get install hostapd 
+printf "[-] instaling dnsmasq. . .\n"
 apt-get install dnsmasq 
+printf "[-] instaling apache2. . .\n"
 apt-get install apache2
+printf "[-] instaling toilet. . .\n"
 apt-get install toilet
+printf "[-] instaling xterm. . .\n"
 apt-get install xterm
-apt-get install dnsspoof
+printf "[-] instaling dsniff. . .\n"
+apt-get install dsniff
+printf "[-] instaling scapy. . .\n"
 pip install scapy
